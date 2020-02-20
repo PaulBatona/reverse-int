a95543f18c3f67b3142e488f4e6c661a48c84f91
PaulBatona <60320870+PaulBatona@users.noreply.github.com>
2020-02-20 22:57:27 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n).split('').reverse().join('');
+    if(n>0){
+        return str;
+    }
+    else {
+        return str.slice(0, -1);
+    }
 }
+
